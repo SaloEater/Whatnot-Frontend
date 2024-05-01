cf9eade7684fb86ab76f0a338702278d077ee755
SaloEater <danil.lazarev@gmail.com>
2024-05-01 18:55:25 -0500

    beautify demo names

--- src/app/demo/[id]/eventComponent.tsx
+++ src/app/demo/[id]/eventComponent.tsx
@@ -16,7 +16,7 @@ export default function EventComponent(
 
     return <div className={`max-height d-flex dimmed-overlay max-width align-items-center gap-2 ${params.event.customer == params.highlight_username && params.highlight_username != '' ? 'bg-green' : ''}`}>
         <img className='image' src={getTeamImageSrc(params.event.team)} alt={params.event.team}/>
-        <div className={`d-flex overflow-hidden align-items-center justify-content-center customer-text customer-border h-50p w-100p ${params.event.customer == params.highlight_username && params.highlight_username != '' ? 'text-white' : ''}`}>
+        <div className={`d-flex overflow-hidden align-items-center justify-content-center customer-text customer-border h-75p w-100p ${params.event.customer == params.highlight_username && params.highlight_username != '' ? 'text-white' : ''}`}>
             <div className='overflow-hidden'>
                 {params.event.customer}
             </div>
