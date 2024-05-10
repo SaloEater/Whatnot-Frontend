906fc9ceb3ef9097073fd3b11ae8847e81c1cc50
SaloEater <danil.lazarev@gmail.com>
2024-05-09 22:32:46 -0500

    beautify data

--- src/app/break/[id]/dataComponent.tsx
+++ src/app/break/[id]/dataComponent.tsx
@@ -22,8 +22,8 @@ export const DataComponent: FC<DataProps> = (props) => {
     }
 
     return <div className='border border-primary rounded rounded-3 border-1'>
-        {takenTeams.length > 0 && <div className='border border-1 p-1'>Highest bid: <b className='fs-2'>{getHighestBid()}</b></div>}
-        {takenTeams.length > 0 && <div className='border border-1 p-1'>Average bid for <b className='fs-5'>{takenTeams.length}</b> teams: <b className='fs-2'>{getAverageBid()}</b></div>}
-        {takenTeams.length > 0 && <div className='border border-1 p-1'>Total: <b className='fs-2'>{getTotal()}</b></div>}
+        {takenTeams.length > 0 && <div className='border border-1 p-1'>Highest bid: <div><b className='fs-1'>{getHighestBid()} $</b></div></div>}
+        {takenTeams.length > 0 && <div className='border border-1 p-1'>Average bid for <b className='fs-5'>{takenTeams.length}</b> teams: <div><b className='fs-1'>{getAverageBid()} $</b></div></div>}
+        {takenTeams.length > 0 && <div className='border border-1 p-1'>Total: <div><b className='fs-1'>{getTotal()} $</b></div></div>}
     </div>
 }
