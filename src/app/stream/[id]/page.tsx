4391fc6a8d857256027224d9cc91f0e9b29b4733
SaloEater <danil.lazarev@gmail.com>
2024-05-10 17:54:50 -0500

    build

--- src/app/stream/[id]/page.tsx
+++ src/app/stream/[id]/page.tsx
@@ -38,6 +38,7 @@ export default function Page({params} : {params: {id: string}}) {
             end_date: date,
             is_deleted: false,
             high_bid_team: '',
+            giveaway_team: ''
         }
 
         post(getEndpoints().break_add, body)
