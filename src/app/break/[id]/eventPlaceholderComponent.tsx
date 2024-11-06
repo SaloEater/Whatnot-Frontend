083fc14266ed9d2e1289a90f19fd57f694df4546
Micae1o <mrviver335@gmail.com>
2024-11-06 18:10:33 +0200

    fixed whatnotSoldEventPlaceholdersComponent error; added ObjectsControl

--- src/app/break/[id]/eventPlaceholderComponent.tsx
+++ src/app/break/[id]/eventPlaceholderComponent.tsx
@@ -1,116 +1,140 @@
-import {Event} from "@/app/entity/entities";
+import { Event } from "@/app/entity/entities";
 import Image from "next/image";
-import {TextInput} from "@/app/common/textInput";
-import {useEffect, useState} from "react";
-import {getEndpoints, post} from "@/app/lib/backend";
+import { TextInput } from "@/app/common/textInput";
+import { useEffect, useState } from "react";
+import { getEndpoints, post } from "@/app/lib/backend";
 
-export default function EventPlaceholderComponent({params}: {params: {
-    event: Event,
-    updateEventPlaceholder: (event: Event) => void,
-    resetEventPlaceholder: null|((event: Event) => void),
-    selectEventPlaceholder: (event: Event) => void,
-    deselectEventPlaceholder: () => void,
-    isSelected: boolean,
-    isAuto: boolean,
-    inputDisabled: boolean,
-}}) {
-    const [newCustomer, setNewCustomer] = useState(params.event.customer)
-    const [newPrice, setNewPrice] = useState(params.event.price)
+export default function EventPlaceholderComponent({
+  params,
+}: {
+  params: {
+    event: Event;
+    updateEventPlaceholder: (event: Event) => void;
+    resetEventPlaceholder: null | ((event: Event) => void);
+    selectEventPlaceholder: (event: Event) => void;
+    deselectEventPlaceholder: () => void;
+    isSelected: boolean;
+    isAuto: boolean;
+    inputDisabled: boolean;
+  };
+}) {
+  const [newCustomer, setNewCustomer] = useState(params.event.customer);
+  const [newPrice, setNewPrice] = useState(params.event.price);
 
-    useEffect(() => {
-        setNewCustomer(params.event.customer)
-    }, [params.event.customer]);
+  useEffect(() => {
+    setNewCustomer(params.event.customer);
+  }, [params.event.customer]);
 
-    useEffect(() => {
-        setNewPrice(params.event.price)
-    }, [params.event.price]);
+  useEffect(() => {
+    setNewPrice(params.event.price);
+  }, [params.event.price]);
 
-    function getTeamImageSrc(team: string) {
-        return `/images/teams/${team}.webp`;
+  function getTeamImageSrc(team: string) {
+    return `/images/teams/${team}.webp`;
+  }
+
+  function updateCustomer(value: string) {
+    setNewCustomer(value);
+  }
+
+  function saveCustomer() {
+    if (newCustomer == "") {
+      params.deselectEventPlaceholder();
+      return;
     }
+    let newEvent = { ...params.event };
+    newEvent.customer = newCustomer;
+    params.updateEventPlaceholder(newEvent);
+  }
 
-    function updateCustomer(value: string) {
-        setNewCustomer(value)
+  function updatePrice(value: string) {
+    if (value == "" || value == "$") {
+      value = "0";
+    } else {
+      value = value.replace("$", "");
     }
+    setNewPrice(parseInt(value));
+  }
 
-    function saveCustomer() {
-        if (newCustomer == '' ) {
-            params.deselectEventPlaceholder()
-            return
-        }
-        let newEvent = {...params.event}
-        newEvent.customer = newCustomer
-        params.updateEventPlaceholder(newEvent)
+  function savePrice() {
+    let newEvent = { ...params.event };
+    newEvent.price = newPrice;
+    params.updateEventPlaceholder(newEvent);
+  }
+
+  function isChanged() {
+    return params.event.customer != "";
+  }
+
+  function onCheck() {
+    if (params.isSelected) {
+      params.deselectEventPlaceholder();
+    } else if (isChanged()) {
+      params.selectEventPlaceholder(params.event);
     }
+  }
 
-    function updatePrice(value: string) {
-        if (value == '' || value == '$') {
-            value = '0'
-        } else {
-            value = value.replace('$', '')
-        }
-        setNewPrice(parseInt(value))
+  function resetCurrent() {
+    if (params.resetEventPlaceholder == null) {
+      return;
     }
+    setNewCustomer("");
+    setNewPrice(0);
+    params.resetEventPlaceholder(params.event);
+  }
 
-    function savePrice() {
-        let newEvent = {...params.event}
-        newEvent.price = newPrice
-        params.updateEventPlaceholder(newEvent)
-    }
-
-    function isChanged() {
-        return params.event.customer != '';
-    }
-
-    function onCheck() {
-        if (params.isSelected) {
-            params.deselectEventPlaceholder()
-        } else if (isChanged()) {
-            params.selectEventPlaceholder(params.event)
-        }
-    }
-
-    function resetCurrent() {
-        if (params.resetEventPlaceholder == null) {
-            return
-        }
-        setNewCustomer('')
-        setNewPrice(0)
-        params.resetEventPlaceholder(params.event)
-    }
-
-    return (
-        <div className='position-relative border border-1 border-primary rounded rounded-3'>
-            <div className='d-flex flex-column justify-content-center align-items-center p-1'>
-                Future event:
-                <div className='d-flex gap-2 flex-column' id={params.isAuto ? 'auto' : ''}>
-                    <div className='d-flex justify-content-evenly align-items-center'>
-                        {params.resetEventPlaceholder != null && <img onClick={resetCurrent} className='bg-secondary p-1 w-15p rounded rounded-3' alt='Delete' src="/images/bin_static_sm.png"/>}
-                        <div className='w-50p'><TextInput
-                            value={`$${newPrice}`}
-                            update={updatePrice}
-                            save={savePrice}
-                            placeholder={'Enter price'}
-                            font_size={null}
-                            onClick={null}
-                            onBlur={null}
-                            disabled={params.inputDisabled}
-                        /></div>
-                        <button className={`btn btn-sm ${params.isSelected ? 'btn-primary' : 'btn-secondary'}`} disabled={!isChanged()} onClick={onCheck}>{params.isSelected ? 'Stop' : 'Copy'}</button>
-                        {/*<label>Copy</label><input disabled={!isChanged()} type='checkbox' checked={params.isSelected} onChange={onCheck}/>*/}
-                    </div>
-                    <TextInput
-                        value={newCustomer}
-                        update={updateCustomer}
-                        save={saveCustomer}
-                        placeholder={'Enter nickname'}
-                        font_size={null}
-                        onClick={null}
-                        onBlur={null}
-                        disabled={params.inputDisabled}
-                    />
-                </div>
+  return (
+    <div className="position-relative border border-1 border-primary rounded rounded-3">
+      <div className="d-flex flex-column justify-content-center align-items-center p-1">
+        Future event:
+        <div
+          className="d-flex gap-2 flex-column"
+          id={params.isAuto ? "auto" : ""}
+        >
+          <div className="d-flex justify-content-evenly align-items-center">
+            {params.resetEventPlaceholder != null && (
+              <img
+                onClick={resetCurrent}
+                className="bg-secondary p-1 w-15p rounded rounded-3"
+                alt="Delete"
+                src="/images/bin_static_sm.png"
+              />
+            )}
+            <div className="w-50p">
+              <TextInput
+                value={`$${newPrice}`}
+                update={updatePrice}
+                save={savePrice}
+                placeholder={"Enter price"}
+                font_size={null}
+                onClick={null}
+                onBlur={null}
+                disabled={params.inputDisabled}
+              />
             </div>
+            <button
+              className={`btn btn-sm ${
+                params.isSelected ? "btn-primary" : "btn-secondary"
+              }`}
+              disabled={!isChanged()}
+              onClick={onCheck}
+            >
+              {params.isSelected ? "Stop" : "Copy"}
+            </button>
+            {/*<label>Copy</label><input disabled={!isChanged()} type='checkbox' checked={params.isSelected} onChange={onCheck}/>*/}
+          </div>
+          <TextInput
+            value={newCustomer}
+            update={updateCustomer}
+            save={saveCustomer}
+            placeholder={"Enter nickname"}
+            font_size={null}
+            onClick={null}
+            onBlur={null}
+            disabled={params.inputDisabled}
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/app/obs/manage/[id]/TeamsComponent.tsx
+++ src/app/obs/manage/[id]/TeamsComponent.tsx
@@ -1,251 +1,341 @@
-import {FC, useEffect, useState} from "react";
-import {Logger} from "@/app/entity/logger";
-import {MyOBSWebsocket} from "@/app/entity/my_obs_websocket";
-import {SetupLogosComponent} from "@/app/obs/manage/[id]/SetupLogosComponent";
-import {TabsComponent} from "@/app/component/tabsComponent";
+import { FC, useEffect, useState } from "react";
+import { Logger } from "@/app/entity/logger";
+import { MyOBSWebsocket } from "@/app/entity/my_obs_websocket";
+import { SetupLogosComponent } from "@/app/obs/manage/[id]/SetupLogosComponent";
+import { TabsComponent } from "@/app/component/tabsComponent";
 import {
-    EmptyID, EmptyName, EmptyObsItem,
-    ObsItem,
-    ObsScene, RawObsItem, SimpleAnimations,
-    TeamAnimation,
-    TeamAnimations,
-    TeamLogos
+  EmptyID,
+  EmptyName,
+  EmptyObsItem,
+  ObsItem,
+  ObsScene,
+  RawObsItem,
+  SimpleAnimations,
+  TeamAnimation,
+  TeamAnimations,
+  TeamLogos,
 } from "@/app/entity/entities";
-import {Teams} from "@/app/common/teams";
-import {SceneComponent} from "@/app/obs/manage/[id]/scene_component";
-import {ManageComponent} from "@/app/obs/manage/[id]/manage_component";
-import {StepsComponent} from "@/app/component/stepsComponent";
-import {SetupAnimationComponent} from "@/app/obs/manage/[id]/SetupAnimationComponent";
-import {SetupAnimationsComponent} from "@/app/obs/manage/[id]/SetupAnimationsComponent";
-import {filterOnlyType, RawObsItemTypeImage, RawObsItemTypeMedia} from "@/app/utils/obs_item_utils";
-import {SetupSimpleAnimationsComponent} from "@/app/obs/manage/[id]/SetupSimpleAnimationsComponent";
+import { Teams } from "@/app/common/teams";
+import { SceneComponent } from "@/app/obs/manage/[id]/scene_component";
+import { ManageComponent } from "@/app/obs/manage/[id]/manage_component";
+import { StepsComponent } from "@/app/component/stepsComponent";
+import { SetupAnimationComponent } from "@/app/obs/manage/[id]/SetupAnimationComponent";
+import { SetupAnimationsComponent } from "@/app/obs/manage/[id]/SetupAnimationsComponent";
+import {
+  filterOnlyType,
+  RawObsItemTypeImage,
+  RawObsItemTypeMedia,
+} from "@/app/utils/obs_item_utils";
+import { SetupSimpleAnimationsComponent } from "@/app/obs/manage/[id]/SetupSimpleAnimationsComponent";
+import { ObjectsControl } from "./ObjectsControl";
 
 interface TeamsProps {
-    logger: Logger
-    obs: MyOBSWebsocket
-    streamId: number
+  logger: Logger;
+  obs: MyOBSWebsocket;
+  streamId: number;
 }
 
-const LogosKey = 'logos'
-const AnimationsKey = 'animations'
-const SimpleAnimationsKey = 'simple_animations'
+const LogosKey = "logos";
+const AnimationsKey = "animations";
+const SimpleAnimationsKey = "simple_animations";
 
 export const TeamsComponent: FC<TeamsProps> = (props) => {
-    const [animations, setAnimations] = useState<TeamAnimations>({sceneName: EmptyName, animations: []})
-    const [teamLogos, setTeamLogos] = useState<TeamLogos>({sceneName: EmptyName, logos: []})
-    const [simpleAnimations, setSimpleAnimations] = useState<SimpleAnimations>({sceneName: EmptyName, animations: []})
-    const [obsItemsList, setObsItemsList] = useState<RawObsItem[]>([])
-    const [obsScene, setObsScene] = useState<ObsScene|null>(null)
+  const [animations, setAnimations] = useState<TeamAnimations>({
+    sceneName: EmptyName,
+    animations: [],
+  });
+  const [teamLogos, setTeamLogos] = useState<TeamLogos>({
+    sceneName: EmptyName,
+    logos: [],
+  });
+  const [simpleAnimations, setSimpleAnimations] = useState<SimpleAnimations>({
+    sceneName: EmptyName,
+    animations: [],
+  });
+  const [obsItemsList, setObsItemsList] = useState<RawObsItem[]>([]);
+  const [obsScene, setObsScene] = useState<ObsScene | null>(null);
 
-    function createEmptyTeamLogos() {
-        let teamLogos: TeamLogos = {sceneName: EmptyName, logos: []}
-        Teams.forEach((i, j) => {
-            teamLogos.logos.push({obsItem: EmptyObsItem, team: i})
-        })
-        updateLogosStorage(teamLogos)
-        return teamLogos;
+  function createEmptyTeamLogos() {
+    let teamLogos: TeamLogos = { sceneName: EmptyName, logos: [] };
+    Teams.forEach((i, j) => {
+      teamLogos.logos.push({ obsItem: EmptyObsItem, team: i });
+    });
+    updateLogosStorage(teamLogos);
+    return teamLogos;
+  }
+
+  function initTeamLogos(): TeamLogos {
+    let key = LogosKey;
+    if (localStorage.getItem(key) === null) {
+      return createEmptyTeamLogos();
     }
 
-    function initTeamLogos(): TeamLogos {
-        let key = LogosKey
-        if (localStorage.getItem(key) === null) {
-            return createEmptyTeamLogos()
-        }
-
-        let local = localStorage.getItem(key)
-        if (local === null) {
-            let message = 'Team logos are not init'
-            props.logger.add(message)
-            throw new Error(message)
-        }
-
-        return JSON.parse(local)
+    let local = localStorage.getItem(key);
+    if (local === null) {
+      let message = "Team logos are not init";
+      props.logger.add(message);
+      throw new Error(message);
     }
 
-    function createAnimations() {
-        let emptyAnimations: TeamAnimations = {sceneName: EmptyName, animations: []}
-        Teams.forEach((i, j) => {
-            emptyAnimations.animations.push({obsItem: EmptyObsItem, team: i})
-        })
-        updateAnimationStorage(emptyAnimations)
-        return emptyAnimations
+    return JSON.parse(local);
+  }
+
+  function createAnimations() {
+    let emptyAnimations: TeamAnimations = {
+      sceneName: EmptyName,
+      animations: [],
+    };
+    Teams.forEach((i, j) => {
+      emptyAnimations.animations.push({ obsItem: EmptyObsItem, team: i });
+    });
+    updateAnimationStorage(emptyAnimations);
+    return emptyAnimations;
+  }
+
+  function updateTeamLogo(team: string, logo: ObsItem) {
+    setTeamLogos((old) => {
+      let newA = { ...old };
+      let entryIndex = newA.logos.findIndex((i) => i.team == team);
+      if (entryIndex === -1) {
+        return old;
+      }
+      newA.logos[entryIndex].obsItem = logo;
+      return newA;
+    });
+  }
+
+  function updateLogosStorage(value: TeamLogos) {
+    localStorage.setItem(LogosKey, JSON.stringify(value));
+  }
+
+  function updateAnimationStorage(value: TeamAnimations) {
+    localStorage.setItem(AnimationsKey, JSON.stringify(value));
+  }
+
+  function initAnimations(): TeamAnimations {
+    let key = AnimationsKey;
+    if (localStorage.getItem(key) === null) {
+      return createAnimations();
     }
 
-    function updateTeamLogo(team: string, logo: ObsItem) {
-        setTeamLogos((old) => {
-            let newA = {...old}
-            let entryIndex = newA.logos.findIndex(i => i.team == team)
-            if (entryIndex === -1) {
-                return old
-            }
-            newA.logos[entryIndex].obsItem = logo
-            return newA
-        })
+    let local = localStorage.getItem(key);
+    if (local === null) {
+      let message = "Animations are not init";
+      props.logger.add(message);
+      throw new Error(message);
     }
 
-    function updateLogosStorage(value: TeamLogos) {
-        localStorage.setItem(LogosKey, JSON.stringify(value))
+    return JSON.parse(local);
+  }
+
+  function updateSimpleAnimationsStorage(value: SimpleAnimations) {
+    localStorage.setItem(SimpleAnimationsKey, JSON.stringify(value));
+  }
+
+  function createSimpleAnimations() {
+    let simpleAnimationsEmpty: SimpleAnimations = {
+      sceneName: EmptyName,
+      animations: [],
+    };
+    updateSimpleAnimationsStorage(simpleAnimationsEmpty);
+    return simpleAnimationsEmpty;
+  }
+
+  function initSimpleAnimations(): SimpleAnimations {
+    let key = SimpleAnimationsKey;
+    if (localStorage.getItem(key) === null) {
+      return createSimpleAnimations();
     }
 
-    function updateAnimationStorage(value: TeamAnimations) {
-        localStorage.setItem(AnimationsKey, JSON.stringify(value))
+    let local = localStorage.getItem(key);
+    if (local === null) {
+      let message = "Simple Animations are not init";
+      props.logger.add(message);
+      throw new Error(message);
     }
 
-    function initAnimations(): TeamAnimations {
-        let key = AnimationsKey
-        if (localStorage.getItem(key) === null) {
-            return createAnimations()
-        }
+    return JSON.parse(local);
+  }
 
-        let local = localStorage.getItem(key)
-        if (local === null) {
-            let message = 'Animations are not init'
-            props.logger.add(message)
-            throw new Error(message)
-        }
+  useEffect(() => {
+    setAnimations(initAnimations());
+    setTeamLogos(initTeamLogos());
+    setSimpleAnimations(initSimpleAnimations());
+  }, []);
 
-        return JSON.parse(local)
+  useEffect(() => {
+    if (teamLogos.logos.length > 0) {
+      updateLogosStorage(teamLogos);
     }
+  }, [teamLogos]);
 
-    function updateSimpleAnimationsStorage(value: SimpleAnimations) {
-        localStorage.setItem(SimpleAnimationsKey, JSON.stringify(value))
+  useEffect(() => {
+    if (animations.animations.length > 0) {
+      updateAnimationStorage(animations);
     }
+  }, [animations]);
 
-    function createSimpleAnimations() {
-        let simpleAnimationsEmpty: SimpleAnimations = {sceneName: EmptyName, animations: []}
-        updateSimpleAnimationsStorage(simpleAnimationsEmpty)
-        return simpleAnimationsEmpty
+  useEffect(() => {
+    if (simpleAnimations.animations.length > 0) {
+      updateSimpleAnimationsStorage(simpleAnimations);
     }
+  }, [simpleAnimations]);
 
-    function initSimpleAnimations(): SimpleAnimations {
-        let key = SimpleAnimationsKey
-        if (localStorage.getItem(key) === null) {
-            return createSimpleAnimations()
-        }
-
-        let local = localStorage.getItem(key)
-        if (local === null) {
-            let message = 'Simple Animations are not init'
-            props.logger.add(message)
-            throw new Error(message)
-        }
-
-        return JSON.parse(local)
+  useEffect(() => {
+    if (obsScene) {
+      props.obs.getSceneItemList(obsScene).then((r) => {
+        setObsItemsList(r);
+      });
     }
+  }, [obsScene]);
 
-    useEffect(() => {
-        setAnimations(initAnimations())
-        setTeamLogos(initTeamLogos())
-        setSimpleAnimations(initSimpleAnimations())
-    }, []);
+  function updateTeamLogosScene(scene: ObsScene) {
+    setTeamLogos((old) => {
+      let newT = { ...old };
+      newT.sceneName = scene.name;
+      return newT;
+    });
+  }
 
-    useEffect(() => {
-        if (teamLogos.logos.length > 0) {
-            updateLogosStorage(teamLogos)
-        }
-    }, [teamLogos]);
+  function updateAnimationsScene(scene: ObsScene) {
+    setAnimations((old) => {
+      let newT = { ...old };
+      newT.sceneName = scene.name;
+      return newT;
+    });
+  }
 
-    useEffect(() => {
-        if (animations.animations.length > 0) {
-            updateAnimationStorage(animations)
-        }
-    }, [animations]);
+  function updateAnimation(team: string, logo: ObsItem) {
+    setAnimations((old) => {
+      let newA = { ...old };
+      let entryIndex = newA.animations.findIndex((i) => i.team == team);
+      if (entryIndex === -1) {
+        return old;
+      }
+      newA.animations[entryIndex].obsItem = logo;
+      return newA;
+    });
+  }
 
-    useEffect(() => {
-        if (simpleAnimations.animations.length > 0) {
-            updateSimpleAnimationsStorage(simpleAnimations)
-        }
-    }, [simpleAnimations]);
+  function updateSimpleAnimation(id: number, newItem: ObsItem) {
+    setSimpleAnimations((old) => {
+      let newA = { ...old };
+      let entryIndex = newA.animations.findIndex((i) => i.id == id);
+      if (entryIndex === -1) {
+        return old;
+      }
+      newA.animations[entryIndex].obsItem = newItem;
+      return newA;
+    });
+  }
 
-    useEffect(() => {
-        if (obsScene) {
-            props.obs.getSceneItemList(obsScene).then(r => {
-                setObsItemsList(r)
-            })
-        }
-    }, [obsScene]);
+  function updateSimpleAnimationsScene(scene: ObsScene) {
+    setSimpleAnimations((old) => {
+      let newA = { ...old };
+      newA.sceneName = scene.name;
+      return newA;
+    });
+  }
 
-    function updateTeamLogosScene(scene: ObsScene) {
-        setTeamLogos((old) => {
-            let newT = {...old}
-            newT.sceneName = scene.name
-            return newT
-        })
-    }
+  function deleteSimpleAnimation(id: number) {
+    setSimpleAnimations((old) => {
+      let newA = { ...old };
+      let entryIndex = newA.animations.findIndex((i) => i.id == id);
+      if (entryIndex === -1) {
+        return old;
+      }
+      newA.animations.splice(entryIndex, 1);
+      return newA;
+    });
+  }
 
-    function updateAnimationsScene(scene: ObsScene) {
-        setAnimations((old) => {
-            let newT = {...old}
-            newT.sceneName = scene.name
-            return newT
-        })
-    }
-
-    function updateAnimation(team: string, logo: ObsItem) {
-        setAnimations((old) => {
-            let newA = {...old}
-            let entryIndex = newA.animations.findIndex(i => i.team == team)
-            if (entryIndex === -1) {
-                return old
-            }
-            newA.animations[entryIndex].obsItem = logo
-            return newA
-        })
-    }
-
-    function updateSimpleAnimation(id: number, newItem: ObsItem) {
-        setSimpleAnimations((old) => {
-            let newA = {...old}
-            let entryIndex = newA.animations.findIndex(i => i.id == id)
-            if (entryIndex === -1) {
-                return old
-            }
-            newA.animations[entryIndex].obsItem = newItem
-            return newA
-        })
-    }
-
-    function updateSimpleAnimationsScene(scene: ObsScene) {
-        setSimpleAnimations((old) => {
-            let newA = {...old}
-            newA.sceneName = scene.name
-            return newA
-        })
-    }
-
-    function deleteSimpleAnimation(id: number) {
-        setSimpleAnimations((old) => {
-            let newA = {...old}
-            let entryIndex = newA.animations.findIndex(i => i.id == id)
-            if (entryIndex === -1) {
-                return old
-            }
-            newA.animations.splice(entryIndex, 1)
-            return newA
-        })
-    }
-
-    return <div>
-        {
-            props.obs.isConnected() && <SceneComponent obs={props.obs} logger={props.logger} setScene={setObsScene} scene={obsScene}/>
-        }
-        {obsScene && <StepsComponent steps={[
+  return (
+    <div>
+      {props.obs.isConnected() && (
+        <SceneComponent
+          obs={props.obs}
+          logger={props.logger}
+          setScene={setObsScene}
+          scene={obsScene}
+        />
+      )}
+      {obsScene && (
+        <StepsComponent
+          steps={[
             {
-                name: 'Team Logo',
-                node: <SetupLogosComponent updateTeam={updateTeamLogo} itemsList={filterOnlyType(obsItemsList, RawObsItemTypeImage)} scene={obsScene} teamLogos={teamLogos} updateScene={updateTeamLogosScene}/>
+              name: "Animations",
+              node: (
+                <StepsComponent
+                  steps={[
+                    {
+                      name: "Team Logo",
+                      node: (
+                        <SetupLogosComponent
+                          updateTeam={updateTeamLogo}
+                          itemsList={filterOnlyType(
+                            obsItemsList,
+                            RawObsItemTypeImage
+                          )}
+                          scene={obsScene}
+                          teamLogos={teamLogos}
+                          updateScene={updateTeamLogosScene}
+                        />
+                      ),
+                    },
+                    {
+                      name: "Advanced Animations",
+                      node: (
+                        <SetupAnimationsComponent
+                          updateAnimation={updateAnimation}
+                          itemsList={filterOnlyType(
+                            obsItemsList,
+                            RawObsItemTypeMedia
+                          )}
+                          data={animations}
+                          updateScene={updateAnimationsScene}
+                          scene={obsScene}
+                        />
+                      ),
+                    },
+                    {
+                      name: "Simple Animations",
+                      node: (
+                        <SetupSimpleAnimationsComponent
+                          updateAnimation={updateSimpleAnimation}
+                          itemsList={filterOnlyType(
+                            obsItemsList,
+                            RawObsItemTypeMedia
+                          )}
+                          data={simpleAnimations}
+                          updateScene={updateSimpleAnimationsScene}
+                          scene={obsScene}
+                          deleteAnimation={deleteSimpleAnimation}
+                        />
+                      ),
+                    },
+                    {
+                      name: "Stream",
+                      node: (
+                        <ManageComponent
+                          logger={props.logger}
+                          obs={props.obs}
+                          scene={obsScene}
+                          animations={animations}
+                          channelId={props.streamId}
+                        />
+                      ),
+                    },
+                  ]}
+                />
+              ),
             },
             {
-                name: 'Advanced Animations',
-                node: <SetupAnimationsComponent updateAnimation={updateAnimation} itemsList={filterOnlyType(obsItemsList, RawObsItemTypeMedia)} data={animations} updateScene={updateAnimationsScene} scene={obsScene}/>
+              name: "Objects",
+              node: <ObjectsControl obs={props.obs} obsScene={obsScene} />,
             },
-            {
-                name: 'Simple Animations',
-                node: <SetupSimpleAnimationsComponent updateAnimation={updateSimpleAnimation} itemsList={filterOnlyType(obsItemsList, RawObsItemTypeMedia)} data={simpleAnimations} updateScene={updateSimpleAnimationsScene} scene={obsScene} deleteAnimation={deleteSimpleAnimation}/>
-            },
-            {
-                name: 'Stream',
-                node: <ManageComponent logger={props.logger} obs={props.obs} scene={obsScene} animations={animations} channelId={props.streamId} />
-            }
-        ]}/>}
+          ]}
+        />
+      )}
     </div>
-}
+  );
+};
